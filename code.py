64aa6d2b7f9712beed92321e28baefbe2ba5d942
Nanako Ohashi <nanako.ohashi@gmail.com>
2019-12-16 22:01:14 -0700

    feat: update code.py

--- code.py
+++ code.py
@@ -4,3 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sb
 %matplotlib inline
+
+# load Customer Data dataset into a pandas dataframe
+df = pd.read_csv('WA_Fn-UseC_-Telco-Customer-Churn.csv')
+
